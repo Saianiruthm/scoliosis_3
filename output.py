6569054a3c272161dd5c788c40a829b32b6ea224
Saianiruthm <sai2804aniruth@gmail.com>
2024-10-17 17:49:21 +0530

    Added files and folder

---
+++ output.py
@@ -0,0 +1,16 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================== RESTART: E:\task\cnntest.py =========================
+cuda
+resnet18_gradcam.jpg saved successfully.
+Epoch [1/1], Loss: 0.6168, Accuracy: 73.50%
+resnet18 saved successfully.
+densenet121_gradcam.jpg saved successfully.
+Epoch [1/1], Loss: 0.5147, Accuracy: 76.39%
+densenet121 saved successfully.
+Epoch [1/1], Loss: 1.0383, Accuracy: 51.04%
+vit_b_16 saved successfully.
+ResNet - Accuracy: 0.70, Precision: 0.93, Recall: 0.45
+DenseNet - Accuracy: 0.79, Precision: 0.93, Recall: 0.64
+ViT - Accuracy: 0.49, Precision: 0.67, Recall: 0.02
